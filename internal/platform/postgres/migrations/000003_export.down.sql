799096352572a9b6f586199d6597b01c239273bf
Alireza <alireza@novinarz.site>
2026-07-03 20:48:11 +0330

    feat(platform): add migration 000003 export

---
+++ internal/platform/postgres/migrations/000003_export.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS user_export_episodes;
+DROP TABLE IF EXISTS user_export_seasons;
+DROP TABLE IF EXISTS user_export_shows;
+DROP TABLE IF EXISTS user_exports;
+DROP TABLE IF EXISTS shows;
+DROP TYPE IF EXISTS export_status;
